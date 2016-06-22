537007c4ca99da93f2de9f17825f44dc5175a63d
zy <you@example.com>
2016-06-22 16:35:14 +0800

    bbb

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		TextView textView = new TextView(getApplicationContext());
+//		bbb
 	}
 
 }
